40e3b273ded937fe873b10fe5c609d379408675e
MarkNicholas-cpp <marknicholas2002112@gmail.com>
2024-04-05 12:43:26 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { PopularComponent } from './Components/popular/popular.component';
+import { BooksResolver } from './Services/Resolver/books.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'Dashboard', children: [
+      { path: "", redirectTo: "Books", pathMatch: "full" },
+      { path: 'Books', component: DashboardComponent },
+      { path: 'Popular', component: PopularComponent },
+      { path: 'Search', loadChildren:()=> import('./Components/SearchModule/Search.module').then(m => m.SearchModule), resolve: { resolvedOptions: BooksResolver } },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
